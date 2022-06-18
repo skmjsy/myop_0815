7b7bdf0c8e688bdf85302758b0cd19b1c5d1c38b
skmjsy <41653610+skmjsy@users.noreply.github.com>
2022-06-18 14:18:37 -0700

    Update values.py

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -16,58 +16,51 @@ class CarControllerParams:
     self.STEER_DRIVER_FACTOR = 1
 
 class CAR:
-  # genesis
-  GENESIS = "GENESIS 2015-2016"
-  GENESIS_G70 = "GENESIS G70 2018"
-  GENESIS_G80 = "GENESIS G80 2017"
-  GENESIS_EQ900 = "GENESIS EQ900 2017"
-  GENESIS_EQ900_L = "GENESIS EQ900 LIMOUSINE"
-  GENESIS_G90 = "GENESIS G90 2019"
-  # hyundai
-  ELANTRA = "HYUNDAI ELANTRA LIMITED ULTIMATE 2017"
+  # Hyundai
+  ELANTRA = "HYUNDAI ELANTRA 2017"
   ELANTRA_2021 = "HYUNDAI ELANTRA 2021"
-  ELANTRA_HEV_2021 = "HYUNDAI ELANTRA HEV 2021"
+  ELANTRA_HEV_2021 = "HYUNDAI ELANTRA HYBRID 2021"
   ELANTRA_GT_I30 = "HYUNDAI I30 N LINE 2019 & GT 2018 DCT"
-  SONATA = "HYUNDAI SONATA 2020"
-  SONATA_HEV = "HYUNDAI SONATA HEV 2020"
-  SONATA21_HEV = "HYUNDAI SONATA HEV 2021"
-  SONATA19 = "HYUNDAI SONATA 2019"
-  SONATA19_HEV = "HYUNDAI SONATA 2019 HEV"
-  SONATA_LF_TURBO = "HYUNDAI SONATA LF TURBO"
-  KONA = "HYUNDAI KONA 2019"
-  KONA_EV = "HYUNDAI KONA EV 2019"
-  KONA_HEV = "HYUNDAI KONA HEV 2019"
-  IONIQ = "HYUNDAI IONIQ HYBRID PREMIUM 2017"
+  HYUNDAI_GENESIS = "HYUNDAI GENESIS 2015-2016"
+  IONIQ = "HYUNDAI IONIQ HYBRID 2017-2019"
+  IONIQ_HEV_2022 = "HYUNDAI IONIQ HYBRID 2020-2022"
   IONIQ_EV_LTD = "HYUNDAI IONIQ ELECTRIC LIMITED 2019"
   IONIQ_EV_2020 = "HYUNDAI IONIQ ELECTRIC 2020"
+  IONIQ_PHEV_2019 = "HYUNDAI IONIQ PLUG-IN HYBRID 2019"
   IONIQ_PHEV = "HYUNDAI IONIQ PHEV 2020"
-  SANTA_FE = "HYUNDAI SANTA FE LIMITED 2019"
+  KONA = "HYUNDAI KONA 2020"
+  KONA_EV = "HYUNDAI KONA ELECTRIC 2019"
+  KONA_HEV = "HYUNDAI KONA HYBRID 2020"
+  SANTA_FE = "HYUNDAI SANTA FE 2019"
   SANTA_FE_2022 = "HYUNDAI SANTA FE 2022"
   SANTA_FE_HEV_2022 = "HYUNDAI SANTA FE HYBRID 2022"
+  SANTA_FE_PHEV_2022 = "HYUNDAI SANTA FE PlUG-IN HYBRID 2022"
+  SONATA = "HYUNDAI SONATA 2020"
+  SONATA_LF = "HYUNDAI SONATA 2019"
+  TUCSON = "HYUNDAI TUCSON 2019"
   PALISADE = "HYUNDAI PALISADE 2020"
   VELOSTER = "HYUNDAI VELOSTER 2019"
-  GRANDEUR_IG = "HYUNDAI GRANDEUR IG 2017"
-  GRANDEUR_IG_HEV = "HYUNDAI GRANDEUR IG HEV 2019"
-  GRANDEUR_IG_FL = "HYUNDAI GRANDEUR IG FL 2020"
-  GRANDEUR_IG_FL_HEV = "HYUNDAI GRANDEUR IG FL HEV 2020"
-  TUCSON_TL_SCC  = "HYUNDAI TUCSON TL SCC 2017"
-  # kia
-  FORTE = "KIA FORTE E 2018"
-  K5 = "KIA K5 2019 & 2016"
-  K5_2021 = "KIA K5 2021"
-  K5_HEV = "KIA K5 HYBRID 2017 & SPORTS 2019"
-  SPORTAGE = "KIA SPORTAGE S 2020"  
-  SORENTO = "KIA SORENTO GT LINE 2018"
-  STINGER = "KIA STINGER GT2 2018"
-  NIRO_EV = "KIA NIRO EV 2020 PLATINUM"
-  NIRO_HEV = "KIA NIRO HEV 2018"
-  NIRO_HEV_2021 = "KIA NIRO HEV 2021"
-  CEED = "KIA CEED 2019"
-  SELTOS = "KIA SELTOS 2021"
-  MOHAVE = "KIA MOHAVE 2019"
-  K7 = "KIA K7 2016-2019"
-  K7_HEV = "KIA K7 HEV 2016-2019"
-  K9 = "KIA K9 2016-2019"
+  SONATA_HYBRID = "HYUNDAI SONATA HYBRID 2021"
+
+  # Kia
+  KIA_FORTE = "KIA FORTE E 2018 & GT 2021"
+  KIA_K5_2021 = "KIA K5 2021"
+  KIA_NIRO_EV = "KIA NIRO EV 2020"
+  KIA_NIRO_HEV = "KIA NIRO HYBRID 2019"
+  KIA_NIRO_HEV_2021 = "KIA NIRO HYBRID 2021"
+  KIA_OPTIMA = "KIA OPTIMA SX 2019 & 2016"
+  KIA_OPTIMA_H = "KIA OPTIMA HYBRID 2017 & SPORTS 2019"
+  KIA_SELTOS = "KIA SELTOS 2021"
+  KIA_SORENTO = "KIA SORENTO GT LINE 2018"
+  KIA_STINGER = "KIA STINGER GT2 2018"
+  KIA_CEED = "KIA CEED INTRO ED 2019"
+  KIA_EV6 = "KIA EV6 2022"
+
+  # Genesis
+  GENESIS_G70 = "GENESIS G70 2018"
+  GENESIS_G70_2020 = "GENESIS G70 2020"
+  GENESIS_G80 = "GENESIS G80 2017"
+  GENESIS_G90 = "GENESIS G90 2017"
 
 class Buttons:
   NONE = 0
@@ -468,58 +461,46 @@ EV_CAR = {CAR.IONIQ_EV_LTD, CAR.IONIQ_EV_2020, CAR.KONA_EV, CAR.NIRO_EV}
 EV_HYBRID_CAR = EV_CAR | HYBRID_CAR
 
 DBC = {
-  # genesis
-  CAR.GENESIS: dbc_dict('hyundai_kia_generic', None),
-  CAR.GENESIS_G70: dbc_dict('hyundai_kia_generic', None),  
-  CAR.GENESIS_G80: dbc_dict('hyundai_kia_generic', None),
-  CAR.GENESIS_EQ900: dbc_dict('hyundai_kia_generic', None),
-  CAR.GENESIS_EQ900_L: dbc_dict('hyundai_kia_generic', None),
-  CAR.GENESIS_G90: dbc_dict('hyundai_kia_generic', None),
-  # hyundai
   CAR.ELANTRA: dbc_dict('hyundai_kia_generic', None),
   CAR.ELANTRA_2021: dbc_dict('hyundai_kia_generic', None),
   CAR.ELANTRA_HEV_2021: dbc_dict('hyundai_kia_generic', None),
   CAR.ELANTRA_GT_I30: dbc_dict('hyundai_kia_generic', None),
-  CAR.SONATA: dbc_dict('hyundai_kia_generic', None),
-  CAR.SONATA_HEV: dbc_dict('hyundai_kia_generic', None),
-  CAR.SONATA21_HEV: dbc_dict('hyundai_kia_generic', None),
-  CAR.SONATA19: dbc_dict('hyundai_kia_generic', None),
-  CAR.SONATA19_HEV: dbc_dict('hyundai_kia_generic', None),
-  CAR.SONATA_LF_TURBO: dbc_dict('hyundai_kia_generic', None),
+  CAR.GENESIS_G70: dbc_dict('hyundai_kia_generic', None),
+  CAR.GENESIS_G70_2020: dbc_dict('hyundai_kia_generic', None),
+  CAR.GENESIS_G80: dbc_dict('hyundai_kia_generic', None),
+  CAR.GENESIS_G90: dbc_dict('hyundai_kia_generic', None),
+  CAR.HYUNDAI_GENESIS: dbc_dict('hyundai_kia_generic', None),
+  CAR.IONIQ_PHEV_2019: dbc_dict('hyundai_kia_generic', None),
+  CAR.IONIQ_PHEV: dbc_dict('hyundai_kia_generic', None),
+  CAR.IONIQ_EV_2020: dbc_dict('hyundai_kia_generic', None),
+  CAR.IONIQ_EV_LTD: dbc_dict('hyundai_kia_generic', None),
+  CAR.IONIQ: dbc_dict('hyundai_kia_generic', None),
+  CAR.IONIQ_HEV_2022: dbc_dict('hyundai_kia_generic', None),
+  CAR.KIA_FORTE: dbc_dict('hyundai_kia_generic', None),
+  CAR.KIA_K5_2021: dbc_dict('hyundai_kia_generic', None),
+  CAR.KIA_NIRO_EV: dbc_dict('hyundai_kia_generic', None),
+  CAR.KIA_NIRO_HEV: dbc_dict('hyundai_kia_generic', None),
+  CAR.KIA_NIRO_HEV_2021: dbc_dict('hyundai_kia_generic', None),
+  CAR.KIA_OPTIMA: dbc_dict('hyundai_kia_generic', None),
+  CAR.KIA_OPTIMA_H: dbc_dict('hyundai_kia_generic', None),
+  CAR.KIA_SELTOS: dbc_dict('hyundai_kia_generic', None),
+  CAR.KIA_SORENTO: dbc_dict('hyundai_kia_generic', None), # Has 0x5XX messages, but different format
+  CAR.KIA_STINGER: dbc_dict('hyundai_kia_generic', None),
   CAR.KONA: dbc_dict('hyundai_kia_generic', None),
   CAR.KONA_EV: dbc_dict('hyundai_kia_generic', None),
   CAR.KONA_HEV: dbc_dict('hyundai_kia_generic', None),
-  CAR.IONIQ: dbc_dict('hyundai_kia_generic', None),
-  CAR.IONIQ_EV_LTD: dbc_dict('hyundai_kia_generic', None),
-  CAR.IONIQ_PHEV: dbc_dict('hyundai_kia_generic', None),
-  CAR.IONIQ_EV_2020: dbc_dict('hyundai_kia_generic', None),
   CAR.SANTA_FE: dbc_dict('hyundai_kia_generic', None),
   CAR.SANTA_FE_2022: dbc_dict('hyundai_kia_generic', None),
   CAR.SANTA_FE_HEV_2022: dbc_dict('hyundai_kia_generic', None),
+  CAR.SANTA_FE_PHEV_2022: dbc_dict('hyundai_kia_generic', None),
+  CAR.SONATA: dbc_dict('hyundai_kia_generic', None),
+  CAR.SONATA_LF: dbc_dict('hyundai_kia_generic', None), # Has 0x5XX messages, but different format
+  CAR.TUCSON: dbc_dict('hyundai_kia_generic', None),
   CAR.PALISADE: dbc_dict('hyundai_kia_generic', None),
   CAR.VELOSTER: dbc_dict('hyundai_kia_generic', None),
-  CAR.GRANDEUR_IG: dbc_dict('hyundai_kia_generic', None),
-  CAR.GRANDEUR_IG_HEV: dbc_dict('hyundai_kia_generic', None),
-  CAR.GRANDEUR_IG_FL: dbc_dict('hyundai_kia_generic', None),
-  CAR.GRANDEUR_IG_FL_HEV: dbc_dict('hyundai_kia_generic', None),
-  CAR.TUCSON_TL_SCC: dbc_dict('hyundai_kia_generic', None),
-  # kia
-  CAR.FORTE: dbc_dict('hyundai_kia_generic', None),
-  CAR.K5: dbc_dict('hyundai_kia_generic', None),
-  CAR.K5_2021: dbc_dict('hyundai_kia_generic', None),
-  CAR.K5_HEV: dbc_dict('hyundai_kia_generic', None),
-  CAR.SPORTAGE: dbc_dict('hyundai_kia_generic', None),  
-  CAR.SORENTO: dbc_dict('hyundai_kia_generic', None),
-  CAR.STINGER: dbc_dict('hyundai_kia_generic', None),  
-  CAR.NIRO_EV: dbc_dict('hyundai_kia_generic', None),
-  CAR.NIRO_HEV: dbc_dict('hyundai_kia_generic', None),
-  CAR.NIRO_HEV_2021: dbc_dict('hyundai_kia_generic', None),
-  CAR.CEED: dbc_dict('hyundai_kia_generic', None),
-  CAR.SELTOS: dbc_dict('hyundai_kia_generic', None),
-  CAR.MOHAVE: dbc_dict('hyundai_kia_generic', None),
-  CAR.K7: dbc_dict('hyundai_kia_generic', None),
-  CAR.K7_HEV: dbc_dict('hyundai_kia_generic', None),
-  CAR.K9: dbc_dict('hyundai_kia_generic', None),
+  CAR.KIA_CEED: dbc_dict('hyundai_kia_generic', None),
+  CAR.KIA_EV6: dbc_dict('kia_ev6', None),
+  CAR.SONATA_HYBRID: dbc_dict('hyundai_kia_generic', None),
 }
 
 STEER_THRESHOLD = 150
